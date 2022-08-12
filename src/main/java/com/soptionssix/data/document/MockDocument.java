460c32aaa2f83305ecbc10c862b41ee3602ccb0f
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-13 06:06:06 +0900

    feat(mock): Fix MockDocument Id Annotation

--- src/main/java/com/soptionssix/data/document/MockDocument.java
+++ src/main/java/com/soptionssix/data/document/MockDocument.java
@@ -2,14 +2,14 @@ package com.soptionssix.data.document;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
-@Document("mock")
+@Document(collection = "mock")
 @Getter
 @Setter
 public final class MockDocument {
-    @MongoId
+    @Id
     private String _id;
     private String type;
     private String value;
