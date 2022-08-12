e733086528d3bb800a1f76cbd7159e94525f4a46
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-13 06:07:11 +0900

    feat(mock): Add Get All Mock Data Api

--- src/main/java/com/soptionssix/api/contoller/MockController.java
+++ src/main/java/com/soptionssix/api/contoller/MockController.java
@@ -1,6 +1,8 @@
 package com.soptionssix.api.contoller;
 
 import com.soptionssix.api.dto.MockDto;
+import com.soptionssix.domain.service.MockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping("mock")
 public class MockController {
+    private final MockService mockService;
+
+    @Autowired
+    public MockController(MockService mockService) {
+        this.mockService = mockService;
+    }
+
     @GetMapping("get")
     public List<MockDto> getAllMockData() {
-
+        return mockService.getAllMockData();
     }
 }
