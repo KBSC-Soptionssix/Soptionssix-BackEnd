8a9ca8af450f63bbfc20efd884345486aff94eb9
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-13 01:49:26 +0900

    Feat: Init Project

---
+++ src/main/java/com/soptionssix/SoptionssixApplication.java
@@ -0,0 +1,13 @@
+package com.soptionssix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptionssixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoptionssixApplication.class, args);
+    }
+
+}
