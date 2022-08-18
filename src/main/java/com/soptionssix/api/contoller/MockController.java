a35f1828de84e2de3875ba731523ec26e5da0b19
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 12:28:03 +0900

    feat(globalerror): Add Error case test api

--- src/main/java/com/soptionssix/api/contoller/MockController.java
+++ src/main/java/com/soptionssix/api/contoller/MockController.java
@@ -1,9 +1,13 @@
 package com.soptionssix.api.contoller;
 
+import com.soptionssix.api.dto.ErrorResponse;
 import com.soptionssix.api.dto.MockDto;
+import com.soptionssix.domain.error.ExpiredTokenException;
+import com.soptionssix.domain.error.UnauthenticatedException;
 import com.soptionssix.domain.service.MockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,15 @@ public class MockController {
     @GetMapping("get")
     public List<MockDto> getAllMockData() {
         return mockService.getAllMockData();
+    }
+
+    @GetMapping("error/{id}")
+    public ErrorResponse throwError(
+            @PathVariable("id") String id
+    ) {
+        if (id.equals("error1")) {
+            throw new UnauthenticatedException();
+        }
+        throw new ExpiredTokenException();
     }
 }
