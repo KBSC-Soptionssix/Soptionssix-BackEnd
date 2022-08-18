6f17083dc92ba1c1512ae9e7fc1612a254ca5737
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 12:25:24 +0900

    fix: Add Repository Annotation in MockRepository

--- src/main/java/com/soptionssix/data/repository/MockRepository.java
+++ src/main/java/com/soptionssix/data/repository/MockRepository.java
@@ -2,6 +2,8 @@ package com.soptionssix.data.repository;
 
 import com.soptionssix.data.document.MockDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MockRepository extends MongoRepository<MockDocument, String> {
 }
