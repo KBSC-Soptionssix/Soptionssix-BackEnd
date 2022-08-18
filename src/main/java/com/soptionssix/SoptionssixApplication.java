5c0190a0523255658c8bd84b4ef9d2f7039e24c1
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 12:52:12 +0900

    fix(globalerror): servletInitializer 상속 해제
    - war 배포할때 사용하는 친구인데 404 Error 를 핸들링 하기 위해서 DispatcherServlet이 NoHandlerFoundException 을 throw 하도록 설정 하는 과정을 application.properties 로 옮겼기 때문에 사용할 필요 없어짐

--- src/main/java/com/soptionssix/SoptionssixApplication.java
+++ src/main/java/com/soptionssix/SoptionssixApplication.java
@@ -2,12 +2,11 @@ package com.soptionssix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.soptionssix.data")
-public class SoptionssixApplication extends SpringBootServletInitializer {
+public class SoptionssixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SoptionssixApplication.class, args);
