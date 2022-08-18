81927f7f54d5a61cf6358d3b289820e6bde351ab
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 13:11:06 +0900

    feat(globalerror): override handleExceptionInternal
    - 정의한 에러 이외에 Spring 에서 기본적으로 처리하는 Error 도 동일한 format으로 처리하기 위해 handleExceptionInternal override

--- src/main/java/com/soptionssix/api/contoller/GlobalExceptionHandler.java
+++ src/main/java/com/soptionssix/api/contoller/GlobalExceptionHandler.java
@@ -6,10 +6,13 @@ import com.soptionssix.domain.util.ErrorMessage;
 import com.soptionssix.api.dto.ErrorResponse;
 import com.soptionssix.domain.util.ErrorStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -51,5 +54,11 @@ public final class GlobalExceptionHandler extends ResponseEntityExceptionHandler
                 ErrorStatus.INTERNAL_SERVER_ERROR,
                 ErrorMessage.INTERNAL_SERVER_ERROR
         );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        final int internalStatusCode = status.value() * 10;
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
