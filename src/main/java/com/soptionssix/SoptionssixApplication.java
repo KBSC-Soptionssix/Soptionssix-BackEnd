0a0123967cf64d8387ab64ecad340628cacbae64
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 12:27:33 +0900

    fix: 몽고 레포지터리 어노테이션 추가 및 ServletInitializer 상속
    - 몽고 레포지터리 기본 패키지 위치를 알리기 위해 어노테이션 추가
    - 404 Error 를 핸들링 하기 위해서 DispatcherServlet이 NoHandlerFoundException 을 throw 하도록 설정

--- src/main/java/com/soptionssix/SoptionssixApplication.java
+++ src/main/java/com/soptionssix/SoptionssixApplication.java
@@ -2,9 +2,12 @@ package com.soptionssix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class SoptionssixApplication {
+@EnableMongoRepositories(basePackages = "com.soptionssix.data")
+public class SoptionssixApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SoptionssixApplication.class, args);
