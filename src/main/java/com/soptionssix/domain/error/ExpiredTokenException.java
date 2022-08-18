71e1a09d2f7dc7710d2d5c90095d48598aea95e7
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 16:10:32 +0900

    fix(globalerror): Delete Seperated Exception
    - 일반적으로 에러를 sealed class 로 관리하기 때문에 통합하기 위해 외부에 있는 에러들은 삭제

--- src/main/java/com/soptionssix/domain/error/ExpiredTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.soptionssix.domain.error;
-
-import com.soptionssix.domain.util.ErrorMessage;
-
-public class ExpiredTokenException extends IllegalStateException {
-    public ExpiredTokenException() {
-        super(ErrorMessage.EXPIRED_TOKEN);
-    }
-}
--- src/main/java/com/soptionssix/domain/error/UnauthenticatedException.java
+++
@@ -1,9 +0,0 @@
-package com.soptionssix.domain.error;
-
-import com.soptionssix.domain.util.ErrorMessage;
-
-public class UnauthenticatedException extends IllegalArgumentException {
-    public UnauthenticatedException() {
-        super(ErrorMessage.UNAUTHENTICATED);
-    }
-}
