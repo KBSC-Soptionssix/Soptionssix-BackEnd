db73c10e258f82085fd27b445b01aaefabaa8e93
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 16:29:43 +0900

    feat(globalerror): Add All Exception handling

--- src/main/java/com/soptionssix/api/contoller/GlobalExceptionHandler.java
+++ src/main/java/com/soptionssix/api/contoller/GlobalExceptionHandler.java
@@ -1,64 +1,57 @@
 package com.soptionssix.api.contoller;
 
-import com.soptionssix.domain.error.ExpiredTokenException;
-import com.soptionssix.domain.error.UnauthenticatedException;
-import com.soptionssix.domain.util.ErrorMessage;
 import com.soptionssix.api.dto.ErrorResponse;
-import com.soptionssix.domain.util.ErrorStatus;
+import com.soptionssix.domain.error.ErrorCode;
+import com.soptionssix.domain.error.SoptionsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 @Slf4j
 public final class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundUriError() {
-        return new ErrorResponse(
-                ErrorStatus.NOTFOUND,
-                ErrorMessage.NOT_FOUND
-        );
+    @ExceptionHandler(SoptionsException.Unauthenticated.class)
+    public ResponseEntity<Object> handleUnauthenticatedError(SoptionsException.Unauthenticated exception) {
+        return responseErrorFormat(HttpStatus.UNAUTHORIZED, exception);
     }
 
-    @ExceptionHandler(UnauthenticatedException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse handleUnauthenticatedError() {
-        return new ErrorResponse(
-                ErrorStatus.UNAUTHORIZED_UNAUTHENTICATED,
-                ErrorMessage.UNAUTHENTICATED
-        );
-    }
-
-    @ExceptionHandler(ExpiredTokenException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse handleExpiredTokenError() {
-        return new ErrorResponse(
-                ErrorStatus.UNAUTHORIZED_EXPIRED_TOKEN,
-                ErrorMessage.EXPIRED_TOKEN
-        );
+    @ExceptionHandler(SoptionsException.ExpiredToken.class)
+    public ResponseEntity<Object> handleExpiredTokenError(SoptionsException.ExpiredToken exception) {
+        return responseErrorFormat(HttpStatus.UNAUTHORIZED, exception);
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleInternalServerError(final Exception exception) {
+    public ResponseEntity<Object> handleInternalServerError(final Exception exception) {
         log.warn("Internal Error log", exception);
-        return new ErrorResponse(
-                ErrorStatus.INTERNAL_SERVER_ERROR,
-                ErrorMessage.INTERNAL_SERVER_ERROR
-        );
+        SoptionsException soptionsException = new SoptionsException(ErrorCode.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return responseErrorFormat(HttpStatus.INTERNAL_SERVER_ERROR, soptionsException);
     }
 
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        final int internalStatusCode = status.value() * 10;
-        return super.handleExceptionInternal(ex, body, headers, status, request);
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, Object body,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+        SoptionsException soptionsException = new SoptionsException(status.value(), ex.getMessage());
+        return this.responseErrorFormat(status, soptionsException);
+    }
+
+
+    private ResponseEntity<Object> responseErrorFormat(
+            final HttpStatus status,
+            final SoptionsException exception) {
+        return ResponseEntity.status(status.value())
+                .body(createErrorResponseFrom(exception));
+    }
+
+    private ErrorResponse createErrorResponseFrom(SoptionsException exception) {
+        return new ErrorResponse(exception.getErrorCode(), exception.getMessage());
     }
 }
