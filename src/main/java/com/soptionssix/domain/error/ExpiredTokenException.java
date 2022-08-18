d40f10e05b568923304ee84531064de90c7d0c48
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 13:31:26 +0900

    feat(globalerror): set default message in no Arg constructor at ExpiredTokenException

--- src/main/java/com/soptionssix/domain/error/ExpiredTokenException.java
+++ src/main/java/com/soptionssix/domain/error/ExpiredTokenException.java
@@ -1,4 +1,9 @@
 package com.soptionssix.domain.error;
 
+import com.soptionssix.domain.util.ErrorMessage;
+
 public class ExpiredTokenException extends IllegalStateException {
+    public ExpiredTokenException() {
+        super(ErrorMessage.EXPIRED_TOKEN);
+    }
 }
