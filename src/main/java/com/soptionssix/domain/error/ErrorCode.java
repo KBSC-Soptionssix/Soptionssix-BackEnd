0d3dd3e7a77b27d6831211132aba68778df0e48b
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-18 15:47:15 +0900

    fix(globalerror): Fix ErrorStatus to ErrorCode
    - 상태를 관리하기 보다 내부적으로 처리되는 에러 코드를 처리하기 대문에 이름 변경

--- src/main/java/com/soptionssix/domain/error/ErrorCode.java
+++ src/main/java/com/soptionssix/domain/error/ErrorCode.java
@@ -1,12 +1,13 @@
-package com.soptionssix.domain.util;
+package com.soptionssix.domain.error;
 
-public class ErrorStatus {
+public class ErrorCode {
+
     public static final int BAD_REQUEST = 4000;
     public static final int UNAUTHORIZED_UNAUTHENTICATED = 4010;
     public static final int UNAUTHORIZED_EXPIRED_TOKEN = 4011;
     public static final int NOTFOUND = 4040;
     public static final int INTERNAL_SERVER_ERROR = 5000;
 
-    private ErrorStatus() {
+    private ErrorCode() {
     }
 }
