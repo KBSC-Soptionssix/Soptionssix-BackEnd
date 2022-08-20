de91b6f2022f74899319f6518d2478b18e9b5850
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-21 01:04:27 +0900

    feat(entity): remove @Setter in Store

--- src/main/java/com/soptionssix/data/document/Store.java
+++ src/main/java/com/soptionssix/data/document/Store.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "store")
 @Getter
-@Setter
 public class Store {
 
     @Id
@@ -19,6 +18,7 @@ public class Store {
     private String category;
     private String description;
     private int maxDiscount;
+    private int discountStartTime;
     private String phone;
     private int breakStartTime;
     private int breakEndTime;
@@ -30,4 +30,39 @@ public class Store {
     private String mapY;
     private boolean hasChallenge;
 
+    public Store(
+        String photo,
+        String name,
+        String category,
+        String description,
+        int maxDiscount,
+        int discountStartTime,
+        String phone,
+        int breakStartTime,
+        int breakEndTime,
+        int startTime,
+        int endTime,
+        String loadAddress,
+        String address,
+        String mapX,
+        String mapY,
+        boolean hasChallenge
+    ) {
+        this.photo = photo;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.maxDiscount = maxDiscount;
+        this.discountStartTime = discountStartTime;
+        this.phone = phone;
+        this.breakStartTime = breakStartTime;
+        this.breakEndTime = breakEndTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.loadAddress = loadAddress;
+        this.address = address;
+        this.mapX = mapX;
+        this.mapY = mapY;
+        this.hasChallenge = hasChallenge;
+    }
 }
