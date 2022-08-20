bc6aca785e9cd4b568b4bbb926738a4424b9cbc7
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-20 14:13:36 +0900

    fix(entity): Fix Review Document
    - 불필요한 setter 를 생성하지 않도록 변경
    - mongoDB가 자동으로 populate 하도록 @DocumentReference Annotation 추가

--- src/main/java/com/soptionssix/data/document/Review.java
+++ src/main/java/com/soptionssix/data/document/Review.java
@@ -1,28 +1,57 @@
 package com.soptionssix.data.document;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Document(value="review")
+@Document(value = "review")
 @Getter
-@Setter
 public class Review {
+
     @Id
     private String id;
 
-    private String user;
+    @DocumentReference(db = "user")
+    private User user;
 
-    private String receipt;
+    @DocumentReference(db = "receipt")
+    private Receipt receipt;
 
     private String region;
     private String content;
 
     private List<String> photos = new ArrayList<>();
     private Long createdAt;
+
+    public Review(
+        User user, Receipt receipt, String region, String content, List<String> photos
+    ) {
+        this.user = user;
+        this.receipt = receipt;
+        this.region = region;
+        this.content = content;
+        this.photos = photos;
+        this.createdAt = System.currentTimeMillis() / 1000L;
+    }
+
+    public Review(User user, Receipt receipt, String region, String content) {
+        this(user, receipt, region, content, null);
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
+    }
 }
