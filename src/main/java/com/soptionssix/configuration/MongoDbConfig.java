bc2e5c778b9dd352c1c7f5eb1398edc28ce06749
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-20 14:27:06 +0900

    feat(entity): Create MongoDBConfig
    - Spring 에서 Mongo 사용시 _class 필드가 생성되는 것을 제거하는 설정 추가

---
+++ src/main/java/com/soptionssix/configuration/MongoDbConfig.java
@@ -0,0 +1,36 @@
+package com.soptionssix.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoDbConfig {
+
+    private final MongoDatabaseFactory mongoDatabaseFactory;
+    private final MongoMappingContext mongoMappingContext;
+
+    @Autowired
+    public MongoDbConfig(
+        MongoDatabaseFactory mongoDatabaseFactory, MongoMappingContext mongoMappingContext
+    ) {
+        this.mongoDatabaseFactory = mongoDatabaseFactory;
+        this.mongoMappingContext = mongoMappingContext;
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(
+            dbRefResolver, mongoMappingContext
+        );
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+}
