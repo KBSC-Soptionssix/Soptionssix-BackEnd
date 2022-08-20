f543c59e2bc0c477e9c6869f1097fd039abecd53
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-20 13:44:08 +0900

    fix(entity): Fix Product Document
    - 불필요한 setter 를 생성하지 않도록 변경
    - mongoDB가 자동으로 populate 하도록 @DocumentReference Annotation 추가

--- src/main/java/com/soptionssix/data/document/Product.java
+++ src/main/java/com/soptionssix/data/document/Product.java
@@ -1,17 +1,17 @@
 package com.soptionssix.data.document;
 
 import lombok.Getter;
-import lombok.Setter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Document(value="product")
-@Getter
-@Setter
+@Document(value = "product")
+@Getter()
 public class Product {
+
     @Id
     private String id;
-
+    @DocumentReference(db = "store")
     private String store;
 
     private String photo;
@@ -22,4 +22,49 @@ public class Product {
     private int donationCompleteCount;
     private int donationWaitCount;
 
+    public Product(
+        String store, String photo, String name, int stockCount, int price, int discount
+    ) {
+        this.store = store;
+        this.photo = photo;
+        this.name = name;
+        this.stockCount = stockCount;
+        this.price = price;
+        this.discount = discount;
+        this.donationCompleteCount = 0;
+        this.donationWaitCount = 0;
+    }
+
+    public Product(String store, String name, int stockCount, int price, int discount) {
+        this(store, null, name, stockCount, price, discount);
+    }
+
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStockCount(int stockCount) {
+        this.stockCount = stockCount;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public void setDonationCompleteCount(int donationCompleteCount) {
+        this.donationCompleteCount = donationCompleteCount;
+    }
+
+    public void setDonationWaitCount(int donationWaitCount) {
+        this.donationWaitCount = donationWaitCount;
+    }
 }
