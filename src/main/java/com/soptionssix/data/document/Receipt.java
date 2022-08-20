f92eacc1313faafd22ac9f618451ae032d8c447e
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-20 14:08:45 +0900

    fix(entity): Fix Receipt Document
    - 불필요한 setter 를 생성하지 않도록 변경
    - mongoDB가 자동으로 populate 하도록 @DocumentReference Annotation 추가

--- src/main/java/com/soptionssix/data/document/Receipt.java
+++ src/main/java/com/soptionssix/data/document/Receipt.java
@@ -1,31 +1,52 @@
 package com.soptionssix.data.document;
 
 import lombok.Getter;
-import lombok.Setter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Document(value="receipt")
+@Document(value = "receipt")
 @Getter
-@Setter
 public class Receipt {
+
     @Id
     private String id;
 
+    @DocumentReference(db = "user")
+    private final User user;
 
-    private String user;
+    @DocumentReference(db = "store")
+    private final Store store;
 
-    private String store;
+    @DocumentReference(db = "product")
+    private final Product product;
 
-    private String product;
+    @DocumentReference(db = "review")
+    private Review review;
 
-    private String review;
+    private final int productCount;
+    private final Long date;
+    private final Long pickUpTime;
+    private final String paymentMethod;
+    private final boolean isChallenge;
+    private final boolean isDonate;
 
-    private int productCount;
-    private Long date;
-    private Long pickUpTime;
-    private String paymentMethod;
-    private boolean isChallenge;
-    private boolean isDonate;
+    public Receipt(
+        User user, Product product, int productCount, Long pickUpTime,
+        String paymentMethod, boolean isChallenge, boolean isDonate
+    ) {
+        this.user = user;
+        this.store = product.getStore();
+        this.product = product;
+        this.productCount = productCount;
+        this.date = System.currentTimeMillis() / 1000L;
+        this.pickUpTime = pickUpTime;
+        this.paymentMethod = paymentMethod;
+        this.isChallenge = isChallenge;
+        this.isDonate = isDonate;
+    }
 
+    public void setReview(Review review) {
+        this.review = review;
+    }
 }
