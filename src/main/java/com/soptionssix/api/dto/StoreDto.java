96d3dae5ce4db36643a35093b18a276c629315ed
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-24 21:56:50 +0900

    feat(dto): Create StoreDto.java
    - 가게에 대한 모든 정보를 담는 객체 작성

---
+++ src/main/java/com/soptionssix/api/dto/StoreDto.java
@@ -0,0 +1,37 @@
+package com.soptionssix.api.dto;
+
+import javax.validation.constraints.NotNull;
+import nonapi.io.github.classgraph.json.Id;
+
+public record StoreDto(
+    @Id
+    String id,
+    String photo,
+    @NotNull
+    String name,
+    String category,
+    String description,
+    @NotNull
+    int maxDiscount,
+    int discountStartTime,
+    @NotNull
+    String phone,
+    int breakStartTime,
+    int breakEndTime,
+    @NotNull
+    int startTime,
+    @NotNull
+    int endTime,
+    @NotNull
+    String loadAddress,
+    @NotNull
+    String address,
+    @NotNull
+    String mapX,
+    @NotNull
+    String mapY,
+    @NotNull
+    boolean hasChallenge
+) {
+
+}
