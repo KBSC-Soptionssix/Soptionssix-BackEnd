ab615309b2eb7288474df4fce825b9478c18118d
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-24 21:38:58 +0900

    fix(entity): create protected constructor in User Entity

--- src/main/java/com/soptionssix/data/document/User.java
+++ src/main/java/com/soptionssix/data/document/User.java
@@ -2,19 +2,19 @@ package com.soptionssix.data.document;
 
 
 import lombok.Getter;
-import lombok.Setter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "user")
 @Getter
-@Setter
 public class User {
 
     @Id
     private String id;
 
     private String nickName;
-
     private String phone;
+
+    protected User() {
+    }
 }
