93b0a10e122a9ce3e37b96a03772cccaaee37f50
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-25 15:07:29 +0900

    fix(entity): Product Document 에 NotNull validation 추가

--- src/main/java/com/soptionssix/data/document/Product.java
+++ src/main/java/com/soptionssix/data/document/Product.java
@@ -1,5 +1,6 @@
 package com.soptionssix.data.document;
 
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,12 +13,16 @@ public class Product {
     @Id
     private String id;
     @DocumentReference(db = "store")
+    @NotNull
     private Store store;
-
     private String photo;
+    @NotNull
     private String name;
+    @NotNull
     private int stockCount;
+    @NotNull
     private int price;
+    @NotNull
     private int discount;
     private int donationCompleteCount;
     private int donationWaitCount;
