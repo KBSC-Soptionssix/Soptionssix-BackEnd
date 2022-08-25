eacaeb70fa41b4c1b2a625aa40473cc0f7615090
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-25 15:07:50 +0900

    fix(entity): Receipt Document 에 NotNull validation 추가

--- src/main/java/com/soptionssix/data/document/Receipt.java
+++ src/main/java/com/soptionssix/data/document/Receipt.java
@@ -1,5 +1,6 @@
 package com.soptionssix.data.document;
 
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -24,11 +25,17 @@ public class Receipt {
     @DocumentReference(db = "review")
     private Review review;
 
+    @NotNull
     private int productCount;
+    @NotNull
     private Long date;
+    @NotNull
     private Long pickUpTime;
+    @NotNull
     private String paymentMethod;
+    @NotNull
     private boolean isChallenge;
+    @NotNull
     private boolean isDonate;
 
     protected Receipt() {
