6fecf38ee826f9125833451038a8ef0d28fe4177
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-25 15:05:10 +0900

    fix(dto): StoreDetailDto to DiscountStoreDetailDto

---
+++ src/main/java/com/soptionssix/api/dto/DiscountStoreDetailDto.java
@@ -0,0 +1,67 @@
+package com.soptionssix.api.dto;
+
+import com.soptionssix.data.document.Store;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class DiscountStoreDetailDto {
+
+    @NotNull
+    private final String id;
+    private final String photo;
+    @NotNull
+    private final String name;
+    private final String category;
+    private final String description;
+    @NotNull
+    private final int maxDiscount;
+    private final int discountStartTime;
+    @NotNull
+    private final String phone;
+    private final int breakStartTime;
+    private final int breakEndTime;
+    @NotNull
+    private final int startTime;
+    @NotNull
+    private final int endTime;
+    @NotNull
+    private final String loadAddress;
+    @NotNull
+    private final String address;
+    @NotNull
+    private final String mapX;
+    @NotNull
+    private final String mapY;
+    @NotNull
+    private final boolean hasChallenge;
+    private final List<ProductDto> products;
+
+    private DiscountStoreDetailDto(
+        Store store, List<ProductDto> products
+    ) {
+        this.id = store.getId();
+        this.photo = store.getPhoto();
+        this.name = store.getName();
+        this.category = store.getCategory();
+        this.description = store.getDescription();
+        this.maxDiscount = store.getMaxDiscount();
+        this.discountStartTime = store.getDiscountStartTime();
+        this.phone = store.getPhone();
+        this.breakStartTime = store.getBreakStartTime();
+        this.breakEndTime = store.getBreakEndTime();
+        this.startTime = store.getStartTime();
+        this.endTime = store.getEndTime();
+        this.loadAddress = store.getLoadAddress();
+        this.address = store.getAddress();
+        this.mapX = store.getMapX();
+        this.mapY = store.getMapY();
+        this.hasChallenge = store.isHasChallenge();
+        this.products = products;
+    }
+
+    public static DiscountStoreDetailDto of(Store store, List<ProductDto> products) {
+        return new DiscountStoreDetailDto(store, products);
+    }
+}
