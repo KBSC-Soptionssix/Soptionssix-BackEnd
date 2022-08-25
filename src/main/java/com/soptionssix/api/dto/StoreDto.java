b9626f010ef5f3ba47d373c1da21027fac2c769c
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-25 15:03:08 +0900

    fix(dto): Fix StoreDto record class to class
    자체적인 생성자는 감추고 정적 팩토리 메소드를 사용할 수 있도록 만들기 위해 class 를 사용함.
    단 불변성을 지키기 위해서 내부적으로 final 이며 참조하는 객체또한 불변으로 만듦

--- src/main/java/com/soptionssix/api/dto/StoreDto.java
+++ src/main/java/com/soptionssix/api/dto/StoreDto.java
@@ -1,37 +1,64 @@
 package com.soptionssix.api.dto;
 
+import com.soptionssix.data.document.Store;
 import javax.validation.constraints.NotNull;
-import nonapi.io.github.classgraph.json.Id;
+import lombok.Getter;
 
-public record StoreDto(
-    @Id
-    String id,
-    String photo,
-    @NotNull
-    String name,
-    String category,
-    String description,
-    @NotNull
-    int maxDiscount,
-    int discountStartTime,
-    @NotNull
-    String phone,
-    int breakStartTime,
-    int breakEndTime,
-    @NotNull
-    int startTime,
-    @NotNull
-    int endTime,
-    @NotNull
-    String loadAddress,
-    @NotNull
-    String address,
-    @NotNull
-    String mapX,
-    @NotNull
-    String mapY,
-    @NotNull
-    boolean hasChallenge
-) {
+@Getter
+public class StoreDto {
 
+    @NotNull
+    private final String id;
+    private final String photo;
+    @NotNull
+    private final String name;
+    private final String category;
+    private final String description;
+    @NotNull
+    private final int maxDiscount;
+    private final int discountStartTime;
+    @NotNull
+    private final String phone;
+    private final int breakStartTime;
+    private final int breakEndTime;
+    @NotNull
+    private final int startTime;
+    @NotNull
+    private final int endTime;
+    @NotNull
+    private final String loadAddress;
+    @NotNull
+    private final String address;
+    @NotNull
+    private final String mapX;
+    @NotNull
+    private final String mapY;
+    @NotNull
+    private final boolean hasChallenge;
+
+    private StoreDto(
+        Store store
+    ) {
+        this.id = store.getId();
+        this.photo = store.getPhoto();
+        this.name = store.getName();
+        this.category = store.getCategory();
+        this.description = store.getDescription();
+        this.maxDiscount = store.getMaxDiscount();
+        this.discountStartTime = store.getDiscountStartTime();
+        this.phone = store.getPhone();
+        this.breakStartTime = store.getBreakStartTime();
+        this.breakEndTime = store.getBreakEndTime();
+        this.startTime = store.getStartTime();
+        this.endTime = store.getEndTime();
+        this.loadAddress = store.getLoadAddress();
+        this.address = store.getAddress();
+        this.mapX = store.getMapX();
+        this.mapY = store.getMapY();
+        this.hasChallenge = store.isHasChallenge();
+    }
+
+    public static StoreDto of(Store store) {
+        return new StoreDto(store);
+    }
 }
