730e1566f4e652af0b747ab7d72311c4abe81604
jinsu4755 <parkjinsu4755@gmail.com>
2022-08-25 15:08:16 +0900

    feat(dto): Create ProductDto
    자체적인 생성자는 감추고 정적 팩토리 메소드를 사용할 수 있도록 만들기 위해 class 를 사용함.
    단 불변성을 지키기 위해서 내부적으로 final 이며 참조하는 객체또한 불변으로 만듦

---
+++ src/main/java/com/soptionssix/api/dto/ProductDto.java
@@ -0,0 +1,43 @@
+package com.soptionssix.api.dto;
+
+import com.soptionssix.data.document.Product;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ProductDto {
+
+    @NotNull
+    private final String id;
+    @NotNull
+    private final StorePreviewDto storePreview;
+    private final String photo;
+    @NotNull
+    private final String name;
+    @NotNull
+    private final int stockCount;
+    @NotNull
+    private final int price;
+    @NotNull
+    private final int discount;
+    private final int donationCompleteCount;
+    private final int donationWaitCount;
+
+    private ProductDto(
+        Product product
+    ) {
+        this.id = product.getId();
+        this.storePreview = StorePreviewDto.of(product.getStore());
+        this.photo = product.getPhoto();
+        this.name = product.getName();
+        this.stockCount = product.getStockCount();
+        this.price = product.getPrice();
+        this.discount = product.getDiscount();
+        this.donationCompleteCount = product.getDonationCompleteCount();
+        this.donationWaitCount = product.getDonationWaitCount();
+    }
+
+    public static ProductDto of(Product product) {
+        return new ProductDto(product);
+    }
+}
