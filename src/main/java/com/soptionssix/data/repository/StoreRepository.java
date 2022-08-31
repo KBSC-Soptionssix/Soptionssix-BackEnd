d150925a70baa5dc7921a31547906dd5353144c4
dltjdn <hana40004@naver.com>
2022-08-31 20:04:23 +0900

    feat(store): create StoreRepository Interface

---
+++ src/main/java/com/soptionssix/data/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.soptionssix.data.repository;
+
+import com.soptionssix.data.document.Store;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StoreRepository extends MongoRepository<Store, String> {
+
+
+}
