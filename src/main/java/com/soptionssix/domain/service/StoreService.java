cb4e33fe369d243f8b53b018ad886494ad72a6f5
dltjdn <hana40004@naver.com>
2022-08-31 20:07:27 +0900

    feat(store): create StoreService and RemoteStoreService

---
+++ src/main/java/com/soptionssix/domain/service/StoreService.java
@@ -0,0 +1,9 @@
+package com.soptionssix.domain.service;
+
+import com.soptionssix.api.dto.StoreDto;
+import java.util.List;
+
+public interface StoreService {
+
+    List<StoreDto> getAllStore();
+}
