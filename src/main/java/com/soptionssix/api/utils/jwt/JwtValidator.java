748ded47cb1f0e6199eadbc786f5c401e0708a55
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-03 03:57:59 +0900

    feat(receipt): Add JwtValidator Constructor
    - Jwt 토큰의 유효성 검증을 위해 Token Provider 와 UserService 를 주입받는 생성자 생성
    
    ref: #29

--- src/main/java/com/soptionssix/api/utils/jwt/JwtValidator.java
+++ src/main/java/com/soptionssix/api/utils/jwt/JwtValidator.java
@@ -1,10 +1,23 @@
 package com.soptionssix.api.utils.jwt;
 
+import com.soptionssix.domain.service.UserService;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class JwtValidator {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+
+    @Autowired
+    public JwtValidator(
+        JwtTokenProvider jwtTokenProvider,
+        UserService userService
+    ) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
 }
