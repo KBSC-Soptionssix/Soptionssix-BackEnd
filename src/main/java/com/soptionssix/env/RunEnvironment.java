09379a6f0936e2e2c22c50d29d5615402976538f
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-03 06:24:19 +0900

    feat: Wrapping Environment class
    Environment 를 Wrapping 하여 확장 기능을 수행하는 객체 생성

---
+++ src/main/java/com/soptionssix/env/RunEnvironment.java
@@ -0,0 +1,31 @@
+package com.soptionssix.env;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunEnvironment {
+
+    private final Environment environment;
+    public static final String ENV_PRODUCT = "product";
+    public static final String ENV_DEVELOPMENT = "development";
+
+    @Autowired
+    public RunEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    public boolean isProduct() {
+        String[] activeProfiles = this.environment.getActiveProfiles();
+        return Arrays.stream(activeProfiles)
+            .anyMatch(profile -> profile.equalsIgnoreCase(ENV_PRODUCT));
+    }
+
+    public boolean isDevelopment() {
+        String[] activeProfiles = this.environment.getActiveProfiles();
+        return Arrays.stream(activeProfiles)
+            .anyMatch(profile -> profile.equalsIgnoreCase(ENV_DEVELOPMENT));
+    }
+}
