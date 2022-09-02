106b77c299f4df147380c03b2ff07a40ddef2f57
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-03 03:56:02 +0900

    feat(receipt): Add decodeJwtToken method
    - token 내부 정보를 읽어서 userId 를 뱉어내는 함수 작성
    
    ref: #29

--- src/main/java/com/soptionssix/api/utils/jwt/JwtTokenProvider.java
+++ src/main/java/com/soptionssix/api/utils/jwt/JwtTokenProvider.java
@@ -1,12 +1,20 @@
 package com.soptionssix.api.utils.jwt;
 
+import com.soptionssix.domain.error.SoptionsException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -49,5 +57,36 @@ public class JwtTokenProvider implements InitializingBean {
         Date expiredTime = new Date(currentTime + this.tokenValidityOfMillisecond);
         log.debug("new Token ExpiredTime : " + expiredTime);
         return expiredTime;
+    }
+
+    public String decodeJwtToken(final String token) {
+        validationToken(token);
+        return parseClaimsOfJwt(token).getBody()
+            .get(CLAIM_KEY_OF_USER_ID)
+            .toString();
+    }
+
+    private void validationToken(String token) {
+        if (token == null || StringUtils.isEmpty(token)) {
+            throw new SoptionsException.BadRequest("token is empty");
+        }
+    }
+
+    private JwtParser getSigningJwtParser() {
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build();
+    }
+
+    private Jws<Claims> parseClaimsOfJwt(final String token) {
+        try {
+            return getSigningJwtParser().parseClaimsJws(token);
+        } catch (ExpiredJwtException expiredJwtException) {
+            throw new SoptionsException.ExpiredToken();
+        } catch (MalformedJwtException malformedJwtException) {
+            throw new SoptionsException.BadRequest(malformedJwtException.getMessage());
+        } catch (JwtException jwtException) {
+            throw new SoptionsException.Unauthenticated(jwtException.getMessage());
+        }
     }
 }
