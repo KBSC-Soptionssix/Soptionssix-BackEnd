c59a2895934edc0f7ffdfb8ccb352c74020c0086
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-03 03:30:22 +0900

    feat(receipt): Create JwtValidator
    - 전역에서 Jwt token 을 검증할 수 있는 컴포넌트 생성
    
    ref: #29

---
+++ src/main/java/com/soptionssix/api/utils/jwt/JwtValidator.java
@@ -0,0 +1,10 @@
+package com.soptionssix.api.utils.jwt;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class JwtValidator {
+
+}
