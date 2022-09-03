230dd2b4fc07bab9c1dd6481d068d79b0302714d
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-03 13:48:22 +0900

    feat(receipt): UserId 를 통해 receipt 검색을 위한 메소드 쿼리 추가

--- src/main/java/com/soptionssix/data/repository/ReceiptRepository.java
+++ src/main/java/com/soptionssix/data/repository/ReceiptRepository.java
@@ -1,8 +1,11 @@
 package com.soptionssix.data.repository;
 
 import com.soptionssix.data.document.Receipt;
+import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReceiptRepository extends MongoRepository<Receipt, String> {
 
+    public List<Receipt> findByUser(ObjectId userId);
 }
