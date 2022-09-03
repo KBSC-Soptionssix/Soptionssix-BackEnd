4dcda862541c3c1fd1859ae4e3b0c065999791fb
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-03 23:15:20 +0900

    feat(receipt): Add GET: ReceiptDetail

--- src/main/java/com/soptionssix/api/contoller/ReceiptController.java
+++ src/main/java/com/soptionssix/api/contoller/ReceiptController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,14 @@ public class ReceiptController {
         PayLoad payLoad = this.jwtTokenProvider.decodeJwtPayload(token);
         List<ReceiptDto> receiptDtoList = receiptService.getAllReceiptOf(payLoad.userId());
         return ResponseEntity.ok(receiptDtoList);
+    }
+
+    @GetMapping("{id}")
+    @RequiredJwtToken
+    public ResponseEntity<ReceiptDto> getReceiptById(
+        @PathVariable("id") String id
+    ) {
+        ReceiptDto receiptDto = this.receiptService.getReceiptById(id);
+        return ResponseEntity.ok(receiptDto);
     }
 }
