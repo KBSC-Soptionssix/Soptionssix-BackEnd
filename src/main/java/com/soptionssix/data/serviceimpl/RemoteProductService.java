fb92690930e7b267ac4ee618f973d8a66c012d7b
dltjdn <hana40004@naver.com>
2022-09-04 16:42:49 +0900

    feat(product): create ProductService getAllProduct

---
+++ src/main/java/com/soptionssix/data/serviceimpl/RemoteProductService.java
@@ -0,0 +1,27 @@
+package com.soptionssix.data.serviceimpl;
+
+import com.soptionssix.api.dto.ProductDto;
+import com.soptionssix.data.document.Product;
+import com.soptionssix.data.repository.ProductRepository;
+import com.soptionssix.domain.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RemoteProductService implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public RemoteProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<ProductDto> getAllProduct(){
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+            .map((value) -> ProductDto.of(value))
+            .toList();
+    }
+}
