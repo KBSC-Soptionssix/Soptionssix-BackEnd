734bcd7af0b64e90bddd9371cb20fee4735b6f48
dltjdn <hana40004@naver.com>
2022-09-04 13:49:27 +0900

    feat(store): create StoreService getStoreDetail

--- src/main/java/com/soptionssix/data/serviceimpl/RemoteStoreService.java
+++ src/main/java/com/soptionssix/data/serviceimpl/RemoteStoreService.java
@@ -1,10 +1,13 @@
 package com.soptionssix.data.serviceimpl;
 
+import com.soptionssix.api.dto.StoreDetailDto;
 import com.soptionssix.api.dto.StoreDto;
+import com.soptionssix.data.document.Product;
 import com.soptionssix.data.document.Store;
 import com.soptionssix.data.repository.StoreRepository;
 import com.soptionssix.domain.service.StoreService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +29,10 @@ public class RemoteStoreService implements StoreService {
             .map((value) -> StoreDto.of(value))
             .toList();
     }
+
+    @Override
+    public StoreDetailDto getStoreDetail(String id) {
+
+    }
+
 }
--- src/main/java/com/soptionssix/domain/service/StoreService.java
+++ src/main/java/com/soptionssix/domain/service/StoreService.java
@@ -1,9 +1,12 @@
 package com.soptionssix.domain.service;
 
+import com.soptionssix.api.dto.StoreDetailDto;
 import com.soptionssix.api.dto.StoreDto;
 import java.util.List;
 
 public interface StoreService {
 
     List<StoreDto> getAllStore();
+
+    StoreDetailDto getStoreDetail(String id);
 }
