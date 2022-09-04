d8b997ef32bf4952e7222e7a05f438aee9212d0d
dltjdn <hana40004@naver.com>
2022-09-04 16:41:48 +0900

    feat(product): create ProductController getAllProduct

---
+++ src/main/java/com/soptionssix/api/contoller/ProductController.java
@@ -0,0 +1,29 @@
+package com.soptionssix.api.contoller;
+
+import com.soptionssix.api.dto.ProductDto;
+import com.soptionssix.api.dto.StoreDto;
+import com.soptionssix.domain.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<ProductDto>> getAllProduct() {
+        List<ProductDto> productDtoList = productService.getAllProduct();
+        return ResponseEntity.ok(productDtoList);
+    }
+}
