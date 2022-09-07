86e2eb832f9b42168f8c1f73896cd98c144be6aa
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-07 16:48:21 +0900

    feat(receipt): Add insertReceipt method in ReceiptController

--- src/main/java/com/soptionssix/api/contoller/ReceiptController.java
+++ src/main/java/com/soptionssix/api/contoller/ReceiptController.java
@@ -4,12 +4,15 @@ import com.soptionssix.api.dto.ReceiptDto;
 import com.soptionssix.api.utils.jwt.JwtTokenProvider;
 import com.soptionssix.api.utils.jwt.PayLoad;
 import com.soptionssix.api.utils.jwt.RequiredJwtToken;
+import com.soptionssix.domain.service.ProductService;
 import com.soptionssix.domain.service.ReceiptService;
+import com.soptionssix.domain.service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReceiptController {
 
     private final ReceiptService receiptService;
+    private final UserService userService;
+    private final ProductService productService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public ReceiptController(ReceiptService receiptService, JwtTokenProvider jwtTokenProvider) {
+    public ReceiptController(
+        ReceiptService receiptService,
+        UserService userService, ProductService productService,
+        JwtTokenProvider jwtTokenProvider
+    ) {
         this.receiptService = receiptService;
+        this.userService = userService;
+        this.productService = productService;
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
@@ -44,5 +55,13 @@ public class ReceiptController {
     ) {
         ReceiptDto receiptDto = this.receiptService.getReceiptById(id);
         return ResponseEntity.ok(receiptDto);
+    }
+
+    @PostMapping("")
+    @RequiredJwtToken
+    public ResponseEntity<ReceiptDto> addReceipt(
+        @RequestHeader(value = "token") final String token
+    ) {
+        return ResponseEntity.ok(null);
     }
 }
