10b5e08f698472ce56f6965d6c35bc5fc8e42385
dltjdn <hana40004@naver.com>
2022-09-09 01:22:51 +0900

    feat(discount): create DiscountService getStoreDetail

--- src/main/java/com/soptionssix/data/serviceimpl/RemoteDiscountService.java
+++ src/main/java/com/soptionssix/data/serviceimpl/RemoteDiscountService.java
@@ -1,18 +1,19 @@
 package com.soptionssix.data.serviceimpl;
 
 import com.soptionssix.api.dto.DiscountDto;
+import com.soptionssix.api.dto.DiscountStoreDetailDto;
 import com.soptionssix.api.dto.ProductDto;
-import com.soptionssix.api.dto.StoreDetailDto;
+import com.soptionssix.api.dto.StoreDto;
 import com.soptionssix.data.document.Product;
 import com.soptionssix.data.document.Store;
 import com.soptionssix.data.repository.ProductRepository;
 import com.soptionssix.data.repository.StoreRepository;
+import com.soptionssix.domain.error.SoptionsException;
 import com.soptionssix.domain.service.DiscountService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -43,5 +44,21 @@ public class RemoteDiscountService implements DiscountService {
         List<Product> waitDonationList = waitDonations.stream().limit(5).toList();
 
         return DiscountDto.of(storeList, productList, waitDonationList);
+    }
+
+    @Override
+    public DiscountStoreDetailDto getStoreDetail(String storeId) {
+        Store store = storeRepository.findById(storeId).orElseThrow(
+            () -> {
+                throw new SoptionsException.BadRequest("store id가 잘못되었습니다");
+            }
+        );
+
+        List<Product> productList = productRepository.findByStore(store);
+
+        List<ProductDto> products = productList.stream().map((value) -> ProductDto.of(value))
+            .toList();
+
+        return DiscountStoreDetailDto.of(store, products);
     }
 }
