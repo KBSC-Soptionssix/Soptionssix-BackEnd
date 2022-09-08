c932e857b4783856774b6f7fdce301acba84af59
dltjdn <hana40004@naver.com>
2022-09-09 01:21:36 +0900

    feat(discount): create DiscountController getStoreDetail

--- src/main/java/com/soptionssix/api/contoller/DiscountController.java
+++ src/main/java/com/soptionssix/api/contoller/DiscountController.java
@@ -2,12 +2,14 @@ package com.soptionssix.api.contoller;
 
 
 import com.soptionssix.api.dto.DiscountDto;
+import com.soptionssix.api.dto.DiscountStoreDetailDto;
 import com.soptionssix.api.dto.ProductDto;
 import com.soptionssix.domain.service.DiscountService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,13 @@ public class DiscountController {
     public ResponseEntity<DiscountDto> getDiscount() {
         DiscountDto discountDto = discountService.getDiscount();
         return ResponseEntity.ok(discountDto);
+    }
+
+    @GetMapping("{storeId}")
+    public ResponseEntity<DiscountStoreDetailDto> getStoreDetail(
+        @PathVariable(value = "storeId") final String storeId
+    ) {
+        DiscountStoreDetailDto discountStoreDetailDto = discountService.getStoreDetail(storeId);
+        return ResponseEntity.ok(discountStoreDetailDto);
     }
 }
