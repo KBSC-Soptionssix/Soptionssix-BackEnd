be378beb3333667e6f8cc217dc4eb639f9e5e34a
dltjdn <hana40004@naver.com>
2022-09-09 01:22:51 +0900

    feat(discount): create DiscountService getStoreDetail

--- src/main/java/com/soptionssix/data/repository/ProductRepository.java
+++ src/main/java/com/soptionssix/data/repository/ProductRepository.java
@@ -2,11 +2,15 @@ package com.soptionssix.data.repository;
 
 import com.soptionssix.data.document.Product;
 import com.soptionssix.data.document.Receipt;
+import com.soptionssix.data.document.Store;
 import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
+
     List<Product> findByDonationWaitCountGreaterThan(int i);
+
+    List<Product> findByStore(Store store);
 }
--- src/main/java/com/soptionssix/domain/service/DiscountService.java
+++ src/main/java/com/soptionssix/domain/service/DiscountService.java
@@ -1,8 +1,11 @@
 package com.soptionssix.domain.service;
 
 import com.soptionssix.api.dto.DiscountDto;
+import com.soptionssix.api.dto.DiscountStoreDetailDto;
 
 public interface DiscountService {
 
     DiscountDto getDiscount();
+
+    DiscountStoreDetailDto getStoreDetail(String storeId);
 }
