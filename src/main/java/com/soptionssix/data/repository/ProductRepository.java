d5a3ef391e0c95a9e54a1ca959d917657a9181c6
dltjdn <hana40004@naver.com>
2022-09-08 18:47:34 +0900

    feat(discount): add ProductRepository findByDonationWaitGreaterThan

--- src/main/java/com/soptionssix/data/repository/ProductRepository.java
+++ src/main/java/com/soptionssix/data/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.soptionssix.data.repository;
 
 import com.soptionssix.data.document.Product;
+import com.soptionssix.data.document.Receipt;
+import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
+    List<Product> findByDonationWaitCountGreaterThan(int i);
 }
