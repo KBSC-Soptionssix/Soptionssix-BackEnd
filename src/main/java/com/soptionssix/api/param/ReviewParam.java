7756ee38038331aac7384cc38c313cda79a11f3d
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-11 01:06:48 +0900

    feat(review): Create Request ReviewParam

---
+++ src/main/java/com/soptionssix/api/param/ReviewParam.java
@@ -0,0 +1,19 @@
+package com.soptionssix.api.param;
+
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public record ReviewParam(
+    @NotNull(message = "region is null")
+    String region,
+    @NotNull(message = "receipt is null")
+    String receipt,
+    @Size(max = 300)
+    @NotNull(message = "content is null")
+    String content,
+    @Size(min = 1, max = 2)
+    List<String> photo
+) {
+
+}
