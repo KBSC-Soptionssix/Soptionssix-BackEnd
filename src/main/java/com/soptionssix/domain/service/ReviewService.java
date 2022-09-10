baec1937b776d0ecc7f659fcbf77b185d2be7dc1
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-11 02:01:07 +0900

    feat(review): Add saveReview method
    - 저장을 위한 유저 정보 조회 함수 작성
    - 저장을 위한 교환증 조회 함수 작성
    - 교환증에 리뷰를 연결하기 위한 업데이트 함수 작성

--- src/main/java/com/soptionssix/domain/service/ReviewService.java
+++ src/main/java/com/soptionssix/domain/service/ReviewService.java
@@ -1,9 +1,12 @@
 package com.soptionssix.domain.service;
 
 import com.soptionssix.api.dto.ReviewDto;
+import com.soptionssix.api.param.ReviewParam;
 import java.util.List;
 
 public interface ReviewService {
 
     List<ReviewDto> getAllReview();
+
+    ReviewDto saveReview(String userId, ReviewParam reviewParam);
 }
