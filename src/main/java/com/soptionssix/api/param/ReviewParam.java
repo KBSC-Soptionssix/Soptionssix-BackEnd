4e8014194968a9278fd8857151e45670b5a58419
jinsu4755 <parkjinsu4755@gmail.com>
2022-09-11 01:23:31 +0900

    feat(review): Add reviewParam validate message

--- src/main/java/com/soptionssix/api/param/ReviewParam.java
+++ src/main/java/com/soptionssix/api/param/ReviewParam.java
@@ -9,10 +9,10 @@ public record ReviewParam(
     String region,
     @NotNull(message = "receipt is null")
     String receipt,
-    @Size(max = 300)
+    @Size(max = 300, message = "content size must be between 0 and 300")
     @NotNull(message = "content is null")
     String content,
-    @Size(min = 1, max = 2)
+    @Size(min = 1, max = 2, message = "photo content size must be between 1 and 2")
     List<String> photo
 ) {
 
