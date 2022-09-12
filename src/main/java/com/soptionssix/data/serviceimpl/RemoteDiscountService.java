2d407b2cbf081e956498ca9bfca68c1c1c283df5
dltjdn <hana40004@naver.com>
2022-09-12 18:49:19 +0900

    refactor(discount): seperate getDiscount function

--- src/main/java/com/soptionssix/data/serviceimpl/RemoteDiscountService.java
+++ src/main/java/com/soptionssix/data/serviceimpl/RemoteDiscountService.java
@@ -3,14 +3,15 @@ package com.soptionssix.data.serviceimpl;
 import com.soptionssix.api.dto.DiscountDto;
 import com.soptionssix.api.dto.DiscountStoreDetailDto;
 import com.soptionssix.api.dto.ProductDto;
-import com.soptionssix.api.dto.StoreDto;
 import com.soptionssix.data.document.Product;
 import com.soptionssix.data.document.Store;
 import com.soptionssix.data.repository.ProductRepository;
 import com.soptionssix.data.repository.StoreRepository;
 import com.soptionssix.domain.error.SoptionsException;
 import com.soptionssix.domain.service.DiscountService;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -25,7 +26,7 @@ public class RemoteDiscountService implements DiscountService {
 
     @Autowired
     public RemoteDiscountService(
-        StoreRepository storeRepository, ProductRepository productRepository
+            StoreRepository storeRepository, ProductRepository productRepository
     ) {
         this.storeRepository = storeRepository;
         this.productRepository = productRepository;
@@ -34,30 +35,47 @@ public class RemoteDiscountService implements DiscountService {
     @Override
     public DiscountDto getDiscount() {
 
-        Page<Store> storePage = storeRepository.findAll(PageRequest.of(0, 5));
-        List<Store> storeList = storePage.getContent();
+        List<Store> storeList = getStores(5);
 
-        Page<Product> productPage = productRepository.findAll(PageRequest.of(0, 5));
-        List<Product> productList = productPage.getContent();
+        List<Product> productList = getProducts(5);
 
-        List<Product> waitDonations = productRepository.findByDonationWaitCountGreaterThan(0);
-        List<Product> waitDonationList = waitDonations.stream().limit(5).toList();
-
+        List<Product> waitDonationList = getDonationWaitProducts((5));
         return DiscountDto.of(storeList, productList, waitDonationList);
+    }
+
+    @Override
+    public List<Store> getStores(int pageSize) {
+        Page<Store> storePage = storeRepository.findAll(PageRequest.of(0, pageSize));
+        List<Store> storeList = storePage.getContent();
+        return storeList;
+    }
+
+    @Override
+    public List<Product> getProducts(int pageSize) {
+        Page<Product> productPage = productRepository.findAll(PageRequest.of(0, pageSize));
+        List<Product> productList = productPage.getContent();
+        return productList;
+    }
+
+    @Override
+    public List<Product> getDonationWaitProducts(int pageSize) {
+        List<Product> waitDonations = productRepository.findByDonationWaitCountGreaterThan(0);
+        List<Product> waitDonationList = waitDonations.stream().limit(pageSize).toList();
+        return waitDonationList;
     }
 
     @Override
     public DiscountStoreDetailDto getStoreDetail(String storeId) {
         Store store = storeRepository.findById(storeId).orElseThrow(
-            () -> {
-                throw new SoptionsException.BadRequest("store id가 잘못되었습니다");
-            }
+                () -> {
+                    throw new SoptionsException.BadRequest("store id가 잘못되었습니다");
+                }
         );
 
         List<Product> productList = productRepository.findByStore(store);
 
         List<ProductDto> products = productList.stream().map((value) -> ProductDto.of(value))
-            .toList();
+                .toList();
 
         return DiscountStoreDetailDto.of(store, products);
     }
